2cb590175e64477135b3e03afef87d7df63b199e
redavis22 <redavis22@gmail.com>
2016-04-27 19:17:50 -0400

    added appl stock ticker

--- src/stockPrice.py
+++ src/stockPrice.py
@@ -94,7 +94,7 @@ def main():
     c = GoogleFinanceAPI()
 
     #read the ENV VAR, use GE if 'STOCK' isn't there
-    companyName = os.getenv('STOCK', "GOOG")
+    companyName = os.getenv('STOCK', "APPL")
     print 'company name: '+companyName
 
     #The default MARKET is NASDAQ
