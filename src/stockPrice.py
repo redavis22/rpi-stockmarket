64a55bf60094b982309a1b1d6dc110db982b57a2
redavis22 <redavis22@gmail.com>
2016-04-27 20:02:39 -0400

    Update stockPrice.py

--- src/stockPrice.py
+++ src/stockPrice.py
@@ -94,7 +94,7 @@ def main():
     c = GoogleFinanceAPI()
 
     #read the ENV VAR, use GE if 'STOCK' isn't there
-    companyName = os.getenv('STOCK', "TSLA")
+    companyName = os.getenv('STOCK', "VGENX")
     print 'company name: '+companyName
 
     #The default MARKET is NASDAQ
